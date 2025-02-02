89d3216da12c15e488649b31a7a4e53f9c73def6
Fırat Kızılboğa <firatkizilboga11@gmail.com>
2025-02-02 18:55:35 +0300

    remove junk

--- src/core/RegisterFile/obj_dir/sim_main.d
+++
@@ -1,7 +0,0 @@
-sim_main.o: ../sim_main.cpp \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated_funcs.h \
-  VRegisterFile_tb.h
