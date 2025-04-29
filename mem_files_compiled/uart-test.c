34b89357f13cc502326985e3a04267505e00812d
orhosko <yilmazor21@itu.edu.tr>
2025-04-29 21:31:43 +0300

    Add more tests

--- mem_files_compiled/uart-test.c
+++ mem_files_compiled/uart-test.c
@@ -5,7 +5,23 @@
 #define UART_TX (*(volatile uint8_t *)(UART_BASE + 0x08))
 #define UART_RX (*(volatile uint8_t *)(UART_BASE + 0x0C))
 
+static inline __attribute__((always_inline)) void delay(int n) {
+  volatile int a=0;
+  for(int j = 0; j < n; j++) {
+    a++;
+  }
+}
+
+static inline __attribute__((always_inline)) void debug_a(char a) {
+  delay(10000);
+  while (!(UART_STAT & 0x02))
+    ; // Wait until TX ready
+
+  UART_TX = a;
+}
+
 void uart_write(uint8_t data) {
+  debug_a('4');
   volatile int a=0;
   for(int j = 0; j < 10000; j++) { // ~30k inst
     a++;
@@ -14,6 +30,7 @@ void uart_write(uint8_t data) {
   while (!(UART_STAT & 0x02)) // ~235 inst
     ; // Wait until TX ready
   UART_TX = data;
+  debug_a('5');
 }
 
 uint8_t uart_read() {
@@ -27,7 +44,9 @@ uint8_t uart_read() {
 }
 
 void uart_write_str(const char *str) {
+  debug_a('2');
   while (*str != '\0') {
+    debug_a('3');
     uart_write(*str++);
   }
 }
@@ -45,7 +64,9 @@ void uart_read_str(char *buf, int len) {
 
 char buf[16];
 int main() {
- 
+
+  debug_a('1');
+  
   uart_write_str("Hello\r\n");
   while(1)
     {
