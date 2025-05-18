b78bde2c55867733a91be5926f76a937afd93fba
Orhan Berkay Yılmaz <diamond82berkay@gmail.com>
2025-05-19 02:03:24 +0300

    Separate pipeline registers into a new file

--- src/core/Core.sv
+++ src/core/Core.sv
@@ -1,6 +1,7 @@
 `include "../definitions.svh"
 `include "../controlunit_definitions.svh"
 `include "../alu_definitions.svh"
+`include "./PipelineRegisters.svh"
 
 module Core (
     input wire clk,
@@ -10,110 +11,29 @@ module Core (
     output uart_tx
 );
 
-  typedef struct packed {
-    logic [31:0] instruction;
-    logic [31:0] program_counter;
-  } pipe_reg_if_dr_t;
+  pipe_reg_if_dr_t         pr_if_dr;
+  pipe_reg_id_ex_t         pr_id_ex;
+  pipe_reg_ex_mem_t        pr_ex_mem;
+  pipe_reg_mem_wb_t        pr_mem_wb;
 
-  typedef struct packed {
-    logic [31:0] RF_rdata1;
-    logic [31:0] RF_rdata2;
-    logic [31:0] Immediate_imm;
-    logic [31:0] program_counter;
+  logic             [31:0] program_counter = 32'h80000000;
 
-    // control signals
-    logic [4:0] RF_rsel1;
-    logic [4:0] RF_rsel2;
-    logic [4:0] RF_wsel;
-    logic       RF_wen;
-    logic       branch_taken;
-    logic [1:0] RF_wdata_sel;
-    logic [1:0] mem_sel;
-    logic       DM_read;
-    logic       DM_wen;
-    logic       ALU_OP1_SEL;
-    logic       ALU_OP2_SEL;
-    logic [3:0] ALU_Operation;
-    logic [2:0] branch_condition;
+  logic             [31:0] instruction;
 
-  } pipe_reg_id_ex_t;
-
-  typedef struct packed {
-    logic [31:0] ALU_OUT;
-    logic [31:0] RF_rdata2;
-    logic [31:0] target;
-
-    // control signals
-    logic [4:0] RF_wsel;
-    logic       RF_wen;
-    logic [1:0] RF_wdata_sel;
-    logic [1:0] mem_sel;
-    logic       DM_wen;
-  } pipe_reg_ex_mem_t;
-
-  typedef struct packed {
-    logic [31:0] ALU_OUT;
-    logic [31:0] DM_OUT;
-
-    // control signals
-    logic [4:0] RF_wsel;
-    logic       RF_wen;
-    logic [1:0] RF_wdata_sel;
-  } pipe_reg_mem_wb_t;
-
-  pipe_reg_if_dr_t pr_if_dr;
-  pipe_reg_id_ex_t pr_id_ex;
-  pipe_reg_ex_mem_t pr_ex_mem;
-  pipe_reg_mem_wb_t pr_mem_wb;
-
-
-  logic [1:0] state_counter = 2'b11;
-  always_ff @(posedge clk) begin
-    state_counter <= state_counter + 1;
-  end
-
-  /* synth didn't likey
-  always_ff @(negedge clk) begin
-    state_counter <= state_counter + 1;
-  end
-  */
-
-  logic sig_read_im;
-  logic sig_data_read;
-  logic sig_compute;
-  logic sig_write_back;
-
-  // read inst mem
-  // decode
-  // read register(async)
-  assign sig_read_im = (state_counter == 2'b00);
-
-  // read data memory
-  assign sig_data_read = (state_counter == 2'b01);
-
-  // compute - combinational don't do anything
-  assign sig_compute = (state_counter == 2'b10);
-
-  // write register & memory
-  // update pc
-  // assign sig_write_back = (state_counter == 2'b11);
-  assign sig_write_back = (state_counter == 2'b11);
-
-  logic [31:0] program_counter = 32'h80000000;
-
-  logic [31:0] instruction;
-
-  logic [ 4:0] RF_rsel1;
-  logic [ 4:0] RF_rsel2;
-  logic [ 4:0] RF_wsel;
-  logic        RF_wen;
-  logic        branch_taken;
-  logic        DM_wen;
-  logic [ 1:0] RF_wdata_sel;
-  logic        ALU_OP1_SEL;
-  logic        ALU_OP2_SEL;
-  logic [ 3:0] ALU_Operation;
-  logic [ 2:0] branch_condition;
+  logic             [ 4:0] RF_rsel1;
+  logic             [ 4:0] RF_rsel2;
+  logic             [ 4:0] RF_wsel;
+  logic                    RF_wen;
+  logic                    branch_taken;
+  logic                    branch;
+  logic                    jump;
+  logic                    DM_read;
+  logic                    DM_wen;
+  logic             [ 1:0] RF_wdata_sel;
+  logic                    ALU_OP1_SEL;
+  logic                    ALU_OP2_SEL;
+  logic             [ 3:0] ALU_Operation;
+  logic             [ 2:0] branch_condition;
 
   ControlUnit cu (
       .instruction(instruction),
---
+++ src/core/PipelineRegisters.svh
@@ -0,0 +1,54 @@
+typedef struct packed {
+  logic [31:0] instruction;
+  logic [31:0] program_counter;
+} pipe_reg_if_dr_t;
+
+typedef struct packed {
+  logic [31:0] RF_rdata1;
+  logic [31:0] RF_rdata2;
+  logic [31:0] Immediate_imm;
+  logic [31:0] program_counter;
+  logic [6:0] opcode;
+
+  // control signals
+  logic [4:0] RF_rsel1;
+  logic [4:0] RF_rsel2;
+  logic [4:0] RF_wsel;
+  logic       RF_wen;
+  logic       branch;
+  logic       jump;
+  logic [1:0] RF_wdata_sel;
+  logic [1:0] mem_sel;
+  logic       DM_read;
+  logic       DM_wen;
+  logic       ALU_OP1_SEL;
+  logic       ALU_OP2_SEL;
+  logic [3:0] ALU_Operation;
+  logic [2:0] branch_condition;
+
+} pipe_reg_id_ex_t;
+
+typedef struct packed {
+  logic [31:0] ALU_OUT;
+  logic [31:0] program_counter;
+  logic [31:0] RF_rdata2;
+  logic [31:0] target;
+
+  // control signals
+  logic [4:0] RF_wsel;
+  logic       RF_wen;
+  logic [1:0] RF_wdata_sel;
+  logic [1:0] mem_sel;
+  logic       DM_wen;
+} pipe_reg_ex_mem_t;
+
+typedef struct packed {
+  logic [31:0] ALU_OUT;
+  logic [31:0] MMU_OUT;
+  logic [31:0] program_counter;
+
+  // control signals
+  logic [4:0] RF_wsel;
+  logic       RF_wen;
+  logic [1:0] RF_wdata_sel;
+} pipe_reg_mem_wb_t;
